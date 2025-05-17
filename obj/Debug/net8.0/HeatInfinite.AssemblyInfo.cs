d9162d05a0cf9506f7868570d261b8247c6c04d1
NemahAlsayed <91572873+NemahAlsayed@users.noreply.github.com>
2025-05-17 20:54:32 +0000

    New

--- obj/Debug/net8.0/HeatInfinite.AssemblyInfo.cs
+++ obj/Debug/net8.0/HeatInfinite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeatInfinite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd89df44e9b90fa0b002a2902f6f0f44dab49d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d430d598a110129b96a3c908159cfe6946217a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeatInfinite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeatInfinite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
